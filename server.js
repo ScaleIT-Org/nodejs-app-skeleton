9f5dfc740f6bf00adb717872a9b17b0e9a73990e
Nico Kuhn <kuhn@teco.edu>
2018-02-14 12:55:56 +0100

    updated dependencies

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var express  = require('express');
 var path = require('path');
 var http = require('http');
 var app      = express();                               // create our app w/ express
-var mongoose = require('mongoose');                     // mongoose for mongodb
-var morgan = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var cors = require('cors');
@@ -14,7 +12,6 @@ var cors = require('cors');
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, './www'));
 app.use(express.static(path.join(__dirname, './www')));
-app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
@@ -28,17 +25,9 @@ app.use(function(req, res, next) {
    next();
 });
  
-// Models
-var Review = mongoose.model('Review', {
-    title: String,
-    description: String,
-    rating: Number
-});
- 
 // Routes
 app.get('/', function (req, res)
 {
-	console.log("test")
     res.render('index.html');
 });
  

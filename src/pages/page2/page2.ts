5dfcbc0bbc0b95b51e2fbc03446050cb5136f9ce
Andrei Miclaus <frostyandy2k@yahoo.com>
2018-02-17 13:43:51 +0100

    fixed #13; navigation code for pages

---
+++ src/pages/page2/page2.ts
@@ -0,0 +1,15 @@
+import { Component, ViewChild } from '@angular/core';
+import { NavController } from 'ionic-angular';
+@Component({
+  selector: 'page-2',
+  templateUrl: 'page2.html'
+})
+export class Page2 {
+ 
+    constructor(public navCtrl: NavController) {
+ 
+    }
+ 
+    ionViewDidLoad() {
+    }
+}

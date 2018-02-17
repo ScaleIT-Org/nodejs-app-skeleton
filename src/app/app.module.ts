c4d90278ec302629108d30651cdd120719a5876c
Andrei Miclaus <frostyandy2k@yahoo.com>
2018-02-17 13:43:51 +0100

    fixed #13; navigation code for pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,31 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Page2 } from "../pages/page2/page2";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: [MyApp, HomePage, Page2],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(
+      MyApp,
+      {},
+      // this enables deep linking, i.e. http://localhost:3000/#/page2
+      {
+        links: [
+          { component: HomePage, name: "Home", segment: "home" },
+          { component: Page2, name: "Page2", segment: "page2" }
+        ]
+      }
+      // this enables deep linking - end
+    )
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage, Page2],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}

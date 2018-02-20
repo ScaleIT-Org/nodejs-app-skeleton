703356665b6602d717ea48c2df2526da1065efe7
Nico Kuhn <kuhn@teco.edu>
2018-02-20 15:27:36 +0100

    Fixed pid1 zombie

--- Dockerfile
+++ Dockerfile
@@ -25,3 +25,4 @@ EXPOSE 3000
 
 # using array notation causes node to be PID1 and will not exit properly. Without the array notation the shell forwards the sigterm correctly. 
 CMD ["node", "server.js"]
+#CMD node server.js
--- server.js
+++ server.js
@@ -37,7 +37,26 @@ server.listen(app.get('port'), function () {
 	    console.log('Express server listening on port ' + app.get('port'));
 });
 
-process.on('SIGINT', function() {
-  console.log("Terminating ...")
-    process.exit();
+// quit on ctrl-c when running docker in terminal
+process.on('SIGINT', function onSigint () {
+	console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
+  shutdown();
 });
+
+// quit properly on docker stop
+process.on('SIGTERM', function onSigterm () {
+  console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
+  shutdown();
+})
+
+// shut down server
+function shutdown() {
+  server.close(function onServerClosed (err) {
+    if (err) {
+      console.error(err);
+      process.exitCode = 1;
+		}
+		console.info("Stopping Server ....");
+		process.exit();
+  })
+}
